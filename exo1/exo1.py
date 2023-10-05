1753cc36e56ee7c85a34e246569fbc74e921d4b1
Etsuko2003 <nestoraraba@gmail.com>
2023-10-05 09:55:00 +0100

    exo1 done

--- exo1/exo1.py
+++ exo1/exo1.py
@@ -5,5 +5,15 @@ A new item can be created with Item(price, weight)
     The price and weight of an item can be accessed with
     item.price and item.weight.
     Write the code for this class, with the appropriate constructor.
-Example of code using the class: i = Item(10, 20)
-"""
+Example of code using the class: i = Item(10, 20)"""
+
+
+class Item:
+    def __init__(self, price: float, weight: float):
+        self.price = price
+        self.weight = weight
+
+
+item = Item(10, 20)
+print("Price is", item.price)
+print("Weight is", item.weight)
--- exo2/exo2.py
+++ exo2/exo2.py
@@ -8,23 +8,10 @@ Examples:
     solution('abc', 'd') # returns false
 """
 
-"""
-Create unit test using those cases:
-fixed_tests_True = (
-    ( "samurai", "ai"    ),
-    ( "ninja",   "ja"    ),
-    ( "sensei",  "i"     ),
-    ( "abc",     "abc"   ),
-    ( "abcabc",  "bc"    ),
-    ( "fails",   "ails"  ),
-)
 
-fixed_tests_False = (
-    ( "sumo",    "omo"   ),
-    ( "samurai", "ra"    ),
-    ( "abc",     "abcd"  ),
-    ( "ails",    "fails" ),
-    ( "this",    "fails" ),
-    ( "spam",    "eggs"  )
-)
-"""
+def solution(string1: str, string2: str):
+    return string1.endswith(string2)
+
+
+solution('abc', 'bc')  # returns true
+solution('abc', 'd')  # returns false
