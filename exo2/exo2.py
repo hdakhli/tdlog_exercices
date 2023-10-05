2fd6edc8eaf6eb4bcf5e9b212618c3f92b0b8596
Imy2003 <89407179+Imy2003@users.noreply.github.com>
2023-10-05 11:59:58 +0100

    exo2

--- exo2/exo2.py
+++ exo2/exo2.py
@@ -28,3 +28,29 @@ fixed_tests_False = (
     ( "spam",    "eggs"  )
 )
 """
+def solution(a,b):
+    if a[-len(b):] == b :
+        return True
+    else:
+        return False
+
+fixed_tests_True = (
+    ( "samurai", "ai"    ),
+    ( "ninja",   "ja"    ),
+    ( "sensei",  "i"     ),
+    ( "abc",     "abc"   ),
+    ( "abcabc",  "bc"    ),
+    ( "fails",   "ails"  ),
+)
+fixed_tests_False = (
+    ( "sumo",    "omo"   ),
+    ( "samurai", "ra"    ),
+    ( "abc",     "abcd"  ),
+    ( "ails",    "fails" ),
+    ( "this",    "fails" ),
+    ( "spam",    "eggs"  )
+)
+for i in fixed_tests_True:
+    print (f' {i}:{solution(i[0],i[1])}')
+for i in fixed_tests_False:
+    print (f' {i}:{solution(i[0],i[1])}')
