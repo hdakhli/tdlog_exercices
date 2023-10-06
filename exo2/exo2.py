fed647c008fc9a2118bc013bf425ce2ebaf4b7e3
FaridaODG <farida.ouedraogo@centrale-casablanca.ma>
2023-10-06 13:29:26 +0100

    exo2_Farida

--- exo2/exo2.py
+++ exo2/exo2.py
@@ -28,3 +28,38 @@ fixed_tests_False = (
     ( "spam",    "eggs"  )
 )
 """
+
+import unittest
+
+def solution(s, ending):
+    return s.endswith(ending)
+
+class TestSolution(unittest.TestCase):
+
+    def test_true_cases(self):
+        fixed_tests_True = (
+            ("samurai", "ai"),
+            ("ninja", "ja"),
+            ("sensei", "i"),
+            ("abc", "abc"),
+            ("abcabc", "bc"),
+            ("fails", "ails"),
+        )
+        for s, ending in fixed_tests_True:
+            self.assertTrue(solution(s, ending), f"'{s}' ne se termine pas par '{ending}' mais devrait.")
+
+    def test_false_cases(self):
+        fixed_tests_False = (
+            ("sumo", "omo"),
+            ("samurai", "ra"),
+            ("abc", "abcd"),
+            ("ails", "fails"),
+            ("this", "fails"),
+            ("spam", "eggs"),
+        )
+        for s, ending in fixed_tests_False:
+            self.assertFalse(solution(s, ending), f"'{s}' se termine par '{ending}' mais ne devrait pas.")
+
+if __name__ == '__main__':
+    unittest.main()
+
