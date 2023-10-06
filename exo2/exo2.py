701d9db3cee7d5a0698281dfa92102456b8936f4
adam <ka903465@gmail.com>
2023-10-06 11:11:58 +0100

    td1

--- exo2/exo2.py
+++ exo2/exo2.py
@@ -28,3 +28,40 @@ fixed_tests_False = (
     ( "spam",    "eggs"  )
 )
 """
+from solution_module import solution
+
+import unittest
+
+# Importez votre fonction solution ici
+# from votre_module import solution
+
+class TestSolution(unittest.TestCase):
+    def test_true_cases(self):
+        fixed_tests_true = [
+            ("samurai", "ai"),
+            ("ninja", "ja"),
+            ("sensei", "i"),
+            ("abc", "abc"),
+            ("abcabc", "bc"),
+            ("fails", "ails"),
+        ]
+        for s, ending in fixed_tests_true:
+            with self.subTest(s=s, ending=ending):
+                self.assertTrue(solution(s, ending), f"'{s}' should end with '{ending}' but returned False")
+
+    def test_false_cases(self):
+        fixed_tests_false = [
+            ("sumo", "omo"),
+            ("samurai", "ra"),
+            ("abc", "abcd"),
+            ("ails", "fails"),
+            ("this", "fails"),
+            ("spam", "eggs"),
+        ]
+        for s, ending in fixed_tests_false:
+            with self.subTest(s=s, ending=ending):
+                self.assertFalse(solution(s, ending), f"'{s}' should not end with '{ending}' but returned True")
+
+if __name__ == '__main__':
+    unittest.main()
+
