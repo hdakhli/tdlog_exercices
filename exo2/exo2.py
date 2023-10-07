9cfb3eb8a6e05f600820d59b868bb35c3a6a2bab
axel_peare <axelpeare25@gmail.com>
2023-10-07 20:12:23 +0200

    J'ai fait la première partie de l'exercice2 pour les tests, je n'arrive pas à les faire avec fixed_tests_True et fixed_tests_False néanmoins j'ai pu tester le programme

--- exo2/exo2.py
+++ exo2/exo2.py
@@ -7,8 +7,25 @@ Examples:
     solution('abc', 'bc') # returns true
     solution('abc', 'd') # returns false
 """
+class Str_true:
+    def __init__(self, str1=str, str2=str):
+        self._str1 = str1
+        self._str2 = str2
 
-"""
+    def verify_str_true(self, arg1, arg2):
+        self._arg1 = arg1
+        self._arg2 = arg2
+
+        condition1=self._arg1.endswith(self._arg2)
+        condition2= self._arg2 != ""
+        if condition2:
+            if condition1:
+              return True
+            else:
+              return False 
+
+
+'''
 Create unit test using those cases:
 fixed_tests_True = (
     ( "samurai", "ai"    ),
@@ -17,14 +34,18 @@ fixed_tests_True = (
     ( "abc",     "abc"   ),
     ( "abcabc",  "bc"    ),
     ( "fails",   "ails"  ),
-)
+)'''
+r1=fixed_tests_true=Str_true()
+r2=fixed_tests_false=Str_true()
+print(r1.verify_str_true("samurai", "ai"))
+print(r2.verify_str_true("sumo", "omo"))
 
-fixed_tests_False = (
+'''fixed_tests_False = (
     ( "sumo",    "omo"   ),
     ( "samurai", "ra"    ),
     ( "abc",     "abcd"  ),
     ( "ails",    "fails" ),
     ( "this",    "fails" ),
     ( "spam",    "eggs"  )
-)
-"""
+)'''
+
