6eb8ad2a030b459f91e680583817e49fb37a0a48
Roland <roland.soro@centrale-casablanca.ma>
2023-10-10 02:11:35 +0100

    exo1/Roland_Soro

--- exo1/test_exo1.py
+++ exo1/test_exo1.py
@@ -3,9 +3,14 @@ import unittest
 from exo1 import Item
 
 
+
 class Exo1Test(unittest.TestCase):
 
     def test_item_construction(self):
         item = Item(10, 20)
 
         self.assertEqual(20, item.weight)
+
+
+if __name__ == '__main__':
+    unittest.main()
