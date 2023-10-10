94e34e6e09aea7559c22ccd65666aa0f68b77af2
HIBA-NOKRA <147520519+HIBA-NOKRA@users.noreply.github.com>
2023-10-10 16:51:42 +0200

    Update exo1.py

--- exo1/exo1.py
+++ exo1/exo1.py
@@ -6,4 +6,12 @@ A new item can be created with Item(price, weight)
     item.price and item.weight.
     Write the code for this class, with the appropriate constructor.
 Example of code using the class: i = Item(10, 20)
-"""
+""" 
+class Item:
+    def __init__(self, price, weight):
+        self.price = price
+        self.weight = weight
+        
+item= Item (10, 20)
+print("Price:", item.price)  
+print("Weight:", item.weight)
