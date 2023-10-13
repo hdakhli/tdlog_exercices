4652c58dd8f23f8f95eff7b4ad5f610f9680766b
yousrayk <yousra.yakhou@centrale-casablanca.ma>
2023-10-13 14:11:07 +0100

    2nd try

--- exo2/exo2.py
+++ exo2/exo2.py
@@ -7,6 +7,44 @@ Examples:
     solution('abc', 'bc') # returns true
     solution('abc', 'd') # returns false
 """
+import unittest
+
+def solution(string, ending):
+    return string.endswith(ending)
+
+class TestSolution(unittest.TestCase):
+    def test_true_cases(self):
+        fixed_tests_True = [
+            ( "samurai", "ai"    ),
+            ( "ninja",   "ja"    ),
+            ( "sensei",  "i"     ),
+            ( "abc",     "abc"   ),
+            ( "abcabc",  "bc"    ),
+            ( "fails",   "ails"  ),
+        ]
+        
+        for x in fixed_tests_True:
+            input_string, ending_string = x
+            self.assertTrue(solution(input_string, ending_string))
+
+    def test_neg_cases(self):
+        fixed_tests_False = [
+            ( "sumo",    "omo"   ),
+            ( "samurai", "ra"    ),
+            ( "abc",     "abcd"  ),
+            ( "ails",    "fails" ),
+            ( "this",    "fails" ),
+            ( "spam",    "eggs"  )
+        ]
+        for x in fixed_tests_False:
+            input_string, ending_string = x
+            self.assertFalse(solution(input_string, ending_string))
+
+if __name__ == '__main__':
+    unittest.main()
+
+    
+
 
 """
 Create unit test using those cases:
